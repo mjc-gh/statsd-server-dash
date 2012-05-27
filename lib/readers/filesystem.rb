9c81035855f2a11a7a76561af9d848a13308c5eb
mikeycgto <mikeycgto@gmail.com>
2012-05-26 21:02:33 -0400

    implement filesystem reader

--- lib/readers/filesystem.rb
+++ lib/readers/filesystem.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 module StatsdServer
   module Readers
     class FileSystem
@@ -8,7 +10,33 @@ module StatsdServer
       end
 
       def fetch(metric, datatype, opts)
-        raise NotImplementedError
+        path = determine_filename(datatype, metric, opts[:level])
+        start_ts, stop_ts = opts[:range]
+
+        [].tap do |result|
+          File.open(path, 'r') do |file|
+            while line = file.gets
+              split = line.split
+              ts = split[0].to_i
+
+              if ts >= start_ts && ts <= stop_ts
+                result << [ts * 1000, split[1].to_f]
+              end
+            end
+          end
+        end
+
+      rescue Errno::ENOENT => e
+        return [] # return empty data
+
+      end
+
+      protected
+
+      def determine_filename(*args)
+        hash = Digest::MD5.hexdigest(args * ':')
+
+        File.join(@path, hash[0,2], hash[2,2], hash)
       end
     end
   end
