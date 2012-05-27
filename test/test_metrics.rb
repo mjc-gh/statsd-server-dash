f4ca5dd091a012c4750271b1fed83434df4adfd7
mikeycgto <mikeycgto@gmail.com>
2012-05-26 22:11:34 -0400

    simplify zerofill test

--- test/test_metrics.rb
+++ test/test_metrics.rb
@@ -37,11 +37,13 @@ describe StatsdServer::Dash do
 
       @start = Time.now.to_i
       @metric = 'dash_test'
-
-      create_redis_data :counters, @metric, @start, 5, 10 # 5 pts over 10 sec step
     end
 
 		describe 'range parameter' do
+      before do
+        create_redis_data :counters, @metric, @start, 5, 10 # 5 pts over 10 sec step
+      end
+
 			it 'responds to no range' do
 				get "/counters?metrics[]=#{@metric}"
 
@@ -68,31 +70,28 @@ describe StatsdServer::Dash do
 		describe 'zero fill' do
 			before do
 				# add some more pts so we have a gap
-				create_redis_data :counters, @metric, @start - 60, 1, 10
-				create_redis_data :counters, @metric, @start - 120, 2, 10
+				create_redis_data :counters, @metric, @start - 120, 1, 10
+				create_redis_data :counters, @metric, @start, 1, 10
 			end
 
 			it 'fills in gaps' do
 				get "/counters?metrics[]=#{@metric}"
 				data_pts = json_response['metrics'].first['data']
 
-				# 120 range / 10 sec inclusive interval
-				data_pts.size.must_equal (120 / 10) + 2
+				data_pts.size.must_equal (120 / 10) + 1
 
-				non_zero = data_pts[0,2] + data_pts[7,1] + data_pts[9..-1]
-				zero_fill = data_pts[2,4] + data_pts[5,1]
+        # first and last should be non-zero
+        data_pts.first.last.wont_equal 0
+        data_pts.last.last.wont_equal 0
 
-				non_zero.size.must_equal 8
-				zero_fill.size.must_equal 6
-
-				non_zero.each { |pt| pt.last.wont_equal 0, pt }
-				zero_fill.each { |pt| pt.last.must_equal 0, pt }
+        # rest are zero-fills
+        data_pts[1..-2].each { |pt| pt.last.must_equal 0 }
 			end
 
 			it 'does not fill with no_zero_fill parameter' do
 				get "/counters?metrics[]=#{@metric}&no_zero_fill=1"
 			
-				json_response['metrics'].first['data'].size.must_equal 8
+				json_response['metrics'].first['data'].size.must_equal 2
 			end
 		end
 	end
